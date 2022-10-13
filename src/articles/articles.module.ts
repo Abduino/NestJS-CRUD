1141ee80486604860e870addc91e943d21d109a0
Abduino <abdrehmanredi0@gmail.com>
2022-10-13 18:57:43 +0300

    articles module file added

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { ArticlesController } from './articles.controller';
-
+import { PrismaModule } from 'src/nest/prismanp/prisma.module';
 @Module({
   controllers: [ArticlesController],
-  providers: [ArticlesService]
+  providers: [ArticlesService],
+  imports: [PrismaModule]
 })
 export class ArticlesModule {}
