747811954dcbfbf57525f6673cb016217e09bfaa
Abduino <abdrehmanredi0@gmail.com>
2022-10-13 18:58:03 +0300

    articles service file added

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from 'src/nest/prismanp/prisma.service';
+
 
 @Injectable()
 export class ArticlesService {
+  constructor (private prisma:PrismaService){}
+
   create(createArticleDto: CreateArticleDto) {
     return 'This action adds a new article';
   }
 
   findAll() {
     return `This action returns all articles`;
+    return this.prisma.article.findMany({})
   }
 
   findOne(id: number) {
     return `This action returns a #${id} article`;
   }
-
   update(id: number, updateArticleDto: UpdateArticleDto) {
     return `This action updates a #${id} article`;
   }
