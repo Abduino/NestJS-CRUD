f6577184c64478601c6b3549f70f6d54454c68ef
Abduino <abdrehmanredi0@gmail.com>
2022-10-13 18:57:19 +0300

    articles controller file added

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -3,6 +3,7 @@ import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 
+
 @Controller('articles')
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
