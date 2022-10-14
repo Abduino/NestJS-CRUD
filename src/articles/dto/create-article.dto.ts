12ba078bf33f0754c3a42e622e8b644d083f1775
Abduino <abdrehmanredi0@gmail.com>
2022-10-14 15:34:50 +0300

    create article functionality added

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,1 +1,16 @@
-export class CreateArticleDto {}
+// The @ApiProperty decorators are required to make the class properties visible to the SwaggerModule.
+
+import { ApiProperty } from "@nestjs/swagger"; 
+export class CreateArticleDto {
+    @ApiProperty()
+    title: string;
+
+    @ApiProperty({required:false})
+    description?: string;
+
+    @ApiProperty ()
+    body: string;
+
+    @ApiProperty({required:false, default: false})
+    published?: boolean = false;
+}
