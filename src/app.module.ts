32907e27f56e50d0862d959601c2fa58ae115c43
Abduino <abdrehmanredi0@gmail.com>
2022-10-14 16:20:30 +0300

    tariff module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './nest/prismanp/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
+import { TariffModule } from './tariff/tariff.module';
 
 
 @Module({
-  imports: [PrismaModule, ArticlesModule],
+  imports: [PrismaModule, ArticlesModule, TariffModule],
   controllers: [AppController],
   providers: [AppService],
 })
