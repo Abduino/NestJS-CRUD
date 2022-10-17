67b87a7a253fa3f0f9823a021dc2136caf9a6021
Abduino <abdrehmanredi0@gmail.com>
2022-10-17 17:17:00 +0300

    validation for tariff module done

--- src/tariff/dto/create-tariff.dto.ts
+++ src/tariff/dto/create-tariff.dto.ts
@@ -1,16 +1,28 @@
 import { ApiProperty } from "@nestjs/swagger";
 import internal from "stream";
-
+import {  IsNumber, IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator";
 export class CreateTariffDto {
+    
+    
+    @IsNumber()
+    @IsNotEmpty()
     @ApiProperty()
     initial: number;
 
+    @IsNotEmpty()
+    @IsNumber()
     @ApiProperty()
     price: number; 
 
+    
+    
+    @IsNumber()
+    @IsNotEmpty()
     @ApiProperty()
     commission: number; 
 
+    @IsString()
+    @IsNotEmpty()
     @ApiProperty()
     type: string;
 }
