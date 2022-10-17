9b758eeb37b5dde34d298334900c804f32ad70ab
Abduino <abdrehmanredi0@gmail.com>
2022-10-17 18:14:32 +0300

    tariff dto file updated

--- src/tariff/dto/create-tariff.dto.ts
+++ src/tariff/dto/create-tariff.dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
 import internal from "stream";
 import {  IsNumber, IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
 export class CreateTariffDto {
     
     
